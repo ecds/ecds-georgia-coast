00b937ec7f822fc951455f2958a69f69f484a7a7
saundra.barrett@emory.edu <saundra.barrett@emory.edu>
2016-03-14 13:22:40 -0400

    GEO Json Files

--- js/map.js
+++ js/map.js
@@ -1,59 +1,92 @@
 
 function getLeaflet(lat,lng,zoom) {
 
-    console.log(lat);
-    console.log(lng)
-    console.log(zoom);
-    var map = L.map('map').setView([lat, lng], zoom);
 
-        var OpenStreetMap_BlackAndWhite = L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/{type}/{z}/{x}/{y}.{ext}', {
+        var OpenStreetMap = L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/{type}/{z}/{x}/{y}.{ext}', {
             type: 'map',
             ext: 'jpg',
             attribution: 'Tiles Courtesy of <a href="http://www.mapquest.com/">MapQuest</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-            subdomains: '1234'
+            subdomains: '1234',
+            layers: [baseMaps, overlayMaps]
         });
-        OpenStreetMap_BlackAndWhite.addTo(map);
 
-        //var sapelo_marsh_landing = L.marker([31.41778, -81.29559]).addTo(map);
-        //sapelo_marsh_landing.bindPopup("SAPELO MARSH LANDING");
-        //var sapelo_shell_hammock = L.marker([31.40016, -81.2868]).addTo(map);
+        var OpenStreetMap_BlackAndWhite = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+            maxZoom: 18,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        });
 
-        //map.on('click', onMapClick);
+        
 
-    // console.log(lat);
-    // console.log(lng);
-    // //Map parametrs
-    // var mapOptions = {
-    //     zoom: zoom,
-    //     center: new google.maps.LatLng(lat,lng),
-    //     mapTypeId: google.maps.MapTypeId.HYBRID,
+        var map = L.map('map', {
+            center: [lat, lng],
+            zoom: zoom,
+            layers: [OpenStreetMap]
+        });
 
-    //     mapTypeControl: false,
-    //     mapTypeControlOptions: {
-    //         style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
-    //         position: google.maps.ControlPosition.BOTTOM_CENTER
-    //     },
-    //     panControl: false,
-    //     panControlOptions: {
-    //         position: google.maps.ControlPosition.TOP_RIGHT
-    //     },
-    //     zoomControl: false,
-    //     zoomControlOptions: {
-    //         style: google.maps.ZoomControlStyle.LARGE,
-    //         position: google.maps.ControlPosition.TOP_RIGHT
-    //     },
-    //     scaleControl: false,
-    //     scaleControlOptions: {
-    //         position: google.maps.ControlPosition.TOP_LEFT
-    //     },
-    //     streetViewControl: false,
-    //     streetViewControlOptions: {
-    //         position: google.maps.ControlPosition.LEFT_TOP
-    //     }
-    // }
+        var counties = L.layerGroup([]);
+        $.ajax({
+            type: "POST",
+            url: "geo-json/GAcoastalcounties.json",
+            dataType: 'json',
+            success: function (response) {
+                //geojsonLayer = L.geoJson(response).addTo(map);
+                var GAcoastalcounties = L.geoJson(response, {
+                     style: function (feature) {
+                         
+                     },
+                     onEachFeature: function (feature, layer) {
+                         layer.bindPopup(feature.properties.NAME);
+                         layer.addTo(counties);
+                         var countyLabel = L.divIcon({
+                              // Specify a class name we can refer to in CSS.
+                              className: 'countyLabel '+ feature.properties.NAME,
+                              html: "<div class='label'>"+feature.properties.NAME+"</div>",
+                              iconSize: [0,0]
+                              // Set marker width and height
+                            });
+                         var countyCenter = layer.getBounds().getCenter();
+                         L.marker([countyCenter.lat, countyCenter.lng], {icon: countyLabel}).addTo(counties);
+                     }
+                 })
 
-    // //map
-    // var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-    
+            }
+        });
+
+
+        var sapelo_poi = L.layerGroup([]);
+
+         $.ajax({
+            type: "POST",
+            url: "geo-json/Sapelo_POI.json",
+            dataType: 'json',
+            success: function (response) {
+                geojsonLayer = L.geoJson(response).addTo(sapelo_poi);
+            }
+        });
+
+         var shoreline1857 = L.layerGroup([]);
+
+         $.ajax({
+            type: "POST",
+            url: "geo-json/Coastlines_Storms-selected/Sapelo1857-1870coastline.json",
+            dataType: 'json',
+            success: function (response) {
+                geojsonLayer = L.geoJson(response).addTo(shoreline1857);
+            }
+        });
+        
+      
+        var baseMaps = {
+            "Open Street Map": OpenStreetMap,
+            "Open Street Map Black and White": OpenStreetMap_BlackAndWhite
+        };
+
+        var overlayMaps = {
+            "Counties": counties,
+            "Sapelo POIs" : sapelo_poi,
+            "COASTLINES" : shoreline1857
+        };
+
+        L.control.layers(baseMaps, overlayMaps).addTo(map);
+
 }
-
