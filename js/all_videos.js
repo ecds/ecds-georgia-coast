7b747dbcef72b586b4570506d6019145d8b9fa32
Anandi Salinas <alsalin@emory.edu>
2016-03-16 10:26:56 -0400

    added chocolate plantation, raccoon bluff, and nature trail images for galleries, updated panotours with Tony's content and removed forced inline height (I saw that panotour.css was giving the panotour's their height); updated some typos in the long form content pieces; didn't really mess with the main LESS/CSS (I was originally going to set panoDIV but then deleted those lines after I saw panotour.css)

--- js/all_videos.js
+++ js/all_videos.js
@@ -56,9 +56,13 @@ function getLeaflet(lat,lng,zoom) {
         var sapelo_reynolds_mansion = L.marker([31.39708, -81.27702], {icon: L.AwesomeMarkers.icon({icon: 'camera', prefix: 'fa', markerColor: 'blue', spin:false}) }).bindPopup("Reynolds Mansion").addTo(images);
 
         //sapelo articles
-        var sapelo_cabretta_fossils = L.marker([31.443465, -81.234475], {icon: L.AwesomeMarkers.icon({icon: 'file-text', prefix: 'fa', markerColor: 'yellow', spin:false}) }).bindPopup("Fossils in Progress").addTo(articles);
-        var sapelo_cabretta_lost_barrier = L.marker([31.433713, -81.238634], {icon: L.AwesomeMarkers.icon({icon: 'file-text', prefix: 'fa', markerColor: 'yellow', spin:false}) }).bindPopup("The Lost Barrier Islands of Georgia").addTo(articles);
+        var sapelo_cabretta_fossils = L.marker([31.443465, -81.234475], {icon: L.AwesomeMarkers.icon({icon: 'file-text', prefix: 'fa', markerColor: 'orange', spin:false}) }).bindPopup("Fossils in Progress").addTo(articles);
+        var sapelo_cabretta_lost_barrier = L.marker([31.433713, -81.238634], {icon: L.AwesomeMarkers.icon({icon: 'file-text', prefix: 'fa', markerColor: 'orange', spin:false}) }).bindPopup("The Lost Barrier Islands of Georgia").addTo(articles);
 
+        //panotours
+        var sapelo_cabretta_360 = L.marker([31.448899, -81.235913], {icon: L.AwesomeMarkers.icon({icon: 'image', prefix: 'fa', markerColor: 'green', spin:false}) }).bindPopup("Cabretta Island  360&deg; Panorama").addTo(panotour);
+        var sapelo_cabretta_aerial = L.marker([31.447028, -81.235346], {icon: L.AwesomeMarkers.icon({icon: 'image', prefix: 'fa', markerColor: 'green', spin:false}) }).bindPopup("Cabretta Relict Marsh Aerial Panorama").addTo(panotour);
+       
 
 // Sapelo  Sapelo Lighthouse          
 // Sapelo  Nannygoat Beach        
