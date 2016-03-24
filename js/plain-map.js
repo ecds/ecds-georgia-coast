96df3cf4290c86f1ce198f41bf2a3125775ef1ef
Barrett, Saundra <saundra.barrett@emory.edu>
2016-03-24 15:25:41 -0400

    Add plain-map js that adds marker to map base on getLeaflet parameters and data-type

--- js/plain-map.js
+++ js/plain-map.js
@@ -1,7 +1,24 @@
 
 function getLeaflet(lat,lng,zoom) {
 
+    var contentType = $("body").attr('data-type')
 
+    if (contentType == "article") {
+      var markerStyle = {icon: 'file-text', prefix: 'fa', markerColor: 'orange', spin:false}
+    }
+    else if (contentType = "panotour") {
+      var markerStyle = {icon: 'image', prefix: 'fa', markerColor: 'purple', spin:false}
+    }
+    else if (contentType = "video") {
+      var markerStyle = {icon: 'video-camera', prefix: 'fa', markerColor: 'red', spin:false}
+    }
+    else if (contentType = "images") {
+      var markerStyle = {icon: 'camera', prefix: 'fa', markerColor: 'green', spin:false}
+    }
+    else {
+      var markerStyle = {icon: 'map-marker', prefix: 'fa', markerColor: 'blue', spin:false}
+    }
+    
         var OpenStreetMap = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
             attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012',
             layers: [baseMaps]
@@ -26,7 +43,8 @@ function getLeaflet(lat,lng,zoom) {
             "Open Street Map Black and White": OpenStreetMap_BlackAndWhite
         };
 
-        boundaries.addTo(map)
-        L.control.layers(baseMaps,{collapsed: false}).addTo(map);
+        L.control.layers(baseMaps).addTo(map);
+        //var thisMarker = L.marker([lat, lng], , {icon: L.AwesomeMarkers.icon({icon: 'image', prefix: 'fa', markerColor: 'green', spin:false})).addTo(map);
+        var sapelo_cabretta_360 = L.marker([lat, lng], {icon: L.AwesomeMarkers.icon(markerStyle) }).bindPopup("Cabretta Island  360&deg; Panorama").addTo(map);
 
 }
