07d155fc04dc259ad6358ed259785da21a8884dd
Barrett, Saundra <saundra.barrett@emory.edu>
2016-03-28 14:02:52 -0400

    Added sapelo stories

--- js/on-page-functions.js
+++ js/on-page-functions.js
@@ -35,13 +35,19 @@ function formatIsotope() {
         var thisElement = $(this)
         var thisContentType = $(this).attr("data-content-type")
         if (thisContentType == 'photos') {
-            $(this).find("i").addClass('fa fa-camera')
+            $(this).find("i").addClass('fa fa-camera red')
         }
         else if (thisContentType == 'videos') {
-            $(this).find("i").addClass('fa fa-video-camera')
+            $(this).find("i").addClass('fa fa-video-camera green')
         }
         else if (thisContentType == 'article') {
-            $(this).find("i").addClass('fa fa-file-text-o')
+            $(this).find("i").addClass('fa fa-file-text-o orange')
+        }
+        else if (thisContentType == 'panorama') {
+            $(this).find("i").addClass('fa fa-circle-o-notch purple')
+        }
+        else if (thisContentType == 'photo-gallery') {
+            $(this).find("i").addClass('fa fa-picture-o blue')
         }
         else {
             $(this).find("i").css("display","none")
